f4322c16efcc4e581aaac838eb347f2a6856360d
Mirek Kukla <mirekkukla@gmail.com>
2019-10-12 13:03:45 +0200

    temp hub hack

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 /* jshint node: true */
 "use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongodb = require('mongodb');
 
 const MONGO_SERVER = "localhost:27017";
 const DATABASE = "rynly";
 const PORT = 8081;
 
-const MongoClient = mongodb.MongoClient;
+let MongoClient = mongodb.MongoClient;
 
 // setup db
 const mongoUrl = `mongodb://${MONGO_SERVER}/${DATABASE}`;
-let mongoQuery = function(callback) {
-  MongoClient.connect(mongoUrl, {useNewUrlParser: true}, (err, db) => {
+let mongoQuery = function(callback) { // callback takes db as a param
+  MongoClient.connect(mongoUrl, {useNewUrlParser: true}, (err, raw_db) => {
     if (err) throw err;
+    let db = raw_db.db(DATABASE);
     callback(db);
-    db.close();
+    raw_db.close();
   });
 };
 
@@ -28,13 +29,38 @@ mongoQuery((db) => {
 // setup server
 const app = express();
 
-// app.use("api/v1");
 // app.use("api/v1", router);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+let getHub = function(db) {
+  db.collection("Hubs").find().each(console.log);
+};
+
+mongoQuery(getHub);
+
+let hackHub = function() {
+  const mongoUrl = `mongodb://${MONGO_SERVER}/${DATABASE}`;
+    MongoClient.connect(mongoUrl, {useNewUrlParser: true}, (err, raw_db) => {
+      if (err) throw err;
+
+      let db = raw_db.db(DATABASE);
+      console.log("Getting hubs");
+
+      let zipCodes = new Set();
+      console.log(db.collection('Hubs').find({}, {ZipZones: 1}).each(console.log));
+      raw_db.close();
+    });
+};
+
+// hackHub();
+
+
 app.get("/api/v1/hubs", (req, res) => {
-  res.json({message: "Hello World"});
+  console.log("GET hubs")
+  res.json();
+  // res.json({message: "Hello World"});
 });
 
 app.listen(PORT, () => {
