66bfd5120717ab6ab5831607470f0a3b287621fe
Mirek Kukla <mirekkukla@gmail.com>
2019-10-12 12:30:44 +0200

    basic mongo

--- server.js
+++ server.js
@@ -3,9 +3,29 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 
+const MONGO_SERVER = "localhost:27017";
+const DATABASE = "rynly";
 const PORT = 8081;
+
+const MongoClient = mongodb.MongoClient;
+
+// setup db
+const mongoUrl = `mongodb://${MONGO_SERVER}/${DATABASE}`;
+let mongoQuery = function(callback) {
+  MongoClient.connect(mongoUrl, {useNewUrlParser: true}, (err, db) => {
+    if (err) throw err;
+    callback(db);
+    db.close();
+  });
+};
+
+mongoQuery((db) => {
+  console.log(`Database connection to ${mongoUrl} successful`);
+});
+
+// setup server
 const app = express();
 
 // app.use("api/v1");
@@ -13,10 +33,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get("/api/v1/hubs", (req, res) => {
   res.json({message: "Hello World"});
 });
 
 app.listen(PORT, () => {
-  console.log('Server started');
+  console.log("Express server started");
 });
+
