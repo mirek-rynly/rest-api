2b914677d872f88a18ef9f1edd0ae9641c70bdfa
Mirek Kukla <mirekkukla@gmail.com>
2019-10-14 14:59:57 +0200

    error catching

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ let bodyParser = require('body-parser');
 let mongodb = require('mongodb');
 
 const MONGO_URL = "mongodb://localhost:27017";
+const MONGO_PARAMS = {useNewUrlParser: true, useUnifiedTopology: true};
 const DB_NAME = "rynly";
 const EXPRESS_PORT = 8081;
 
 let MongoClient = mongodb.MongoClient;
 
 // test db connection
-MongoClient.connect(MONGO_URL, {useNewUrlParser: true}, (err, client) => {
-  if (err) throw err;
+MongoClient.connect(MONGO_URL, MONGO_PARAMS, (err, client) => {
+  if (err) throw err; // die if connection is bad on startup
   console.log(`DB_NAME connection to ${MONGO_URL} successful`);
 });
 
@@ -23,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // define routes
-app.get("/api/v1/service_area", (req, res) => {
+app.get("/api/v1/service_area", (req, res, next) => {
   logRequest(req);
   let sourceZip = req.query.source;
   let destinationZip = req.query.destination;
@@ -31,7 +32,15 @@ app.get("/api/v1/service_area", (req, res) => {
   if (!sourceZip || !destinationZip) {
     throw new Error(`Must provide 'source' and 'destination' params, you provided ${JSON.stringify(req.query)}`);
   }
-  checkServiceArea(sourceZip, destinationZip, res);
+
+  checkServiceArea(sourceZip, destinationZip, res, next);
+});
+
+// error catching middleware
+app.use(function(err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
 });
 
 // start server
@@ -42,14 +51,16 @@ app.listen(EXPRESS_PORT, () => {
 
 // HELPERS
 
-let checkServiceArea = (sourceZip, destinationZip, res) => {
-  MongoClient.connect(MONGO_URL, {useNewUrlParser: true}, (err, client) => {
-    if (err) throw err;
-    let db = client.db(DB_NAME);
+let checkServiceArea = (sourceZip, destinationZip, res, next) => {
 
+  MongoClient.connect(MONGO_URL, MONGO_PARAMS, (err, client) => {
+    if (mongoConnectError(err, client, next)) return;
+
+    let db = client.db(DB_NAME);
     let sourceZipQuery = {"ZipZones.ZipCode": sourceZip, "ZipZones": {$exists: true, $ne: null}};
     db.collection("Hubs").countDocuments(sourceZipQuery, (err, sourceCount) => {
-      if (err) throw err;
+      if (mongoConnectError(err, client, next)) return;
+
       console.log(`Zip entry count for '${sourceZip}': ${sourceCount}`);
       if (sourceCount < 1) {
         res.json({ service_availability: false });
@@ -59,7 +70,8 @@ let checkServiceArea = (sourceZip, destinationZip, res) => {
 
       let destZipQuery = {"ZipZones.ZipCode": destinationZip, "ZipZones": {$exists: true, $ne: null}};
       db.collection("Hubs").countDocuments(destZipQuery, (err, destCount) => {
-        if (err) throw err;
+        if (mongoConnectError(err, client, next)) return;
+
         console.log(`Zip entry count for '${destinationZip}': ${destCount}`);
         if (destCount < 1) {
           res.json({ service_availability: false });
@@ -77,6 +89,16 @@ let checkServiceArea = (sourceZip, destinationZip, res) => {
 
 
 // UTILS
+
+let mongoConnectError = (err, client, next) => {
+  if (err) {
+    next(err);
+    if (client) client.close();
+    return true;
+  }
+  return false;
+};
+
 
 function logRequest(req) {
   console.log(`${req.method} ${req.originalUrl} ${getIP(req)}`);
